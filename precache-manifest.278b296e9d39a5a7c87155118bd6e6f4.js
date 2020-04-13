cded5e579f7473d79d677c56604b0117338d27ef
Thomas Vanleynseele <thomas.vanleynseele@student.kdg.be>
2020-04-13 19:03:30 +0200

    Updates

---
+++ precache-manifest.278b296e9d39a5a7c87155118bd6e6f4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee5e3d98e7423748b8dd0d402e7420ba",
+    "url": "/BurgerBuilder/index.html"
+  },
+  {
+    "revision": "2363f3a3ab726c94c670",
+    "url": "/BurgerBuilder/static/css/3.63b6d38c.chunk.css"
+  },
+  {
+    "revision": "e7f0034e654e14cd74d0",
+    "url": "/BurgerBuilder/static/css/4.239737ca.chunk.css"
+  },
+  {
+    "revision": "9903bbdc8dbfb9af23f5",
+    "url": "/BurgerBuilder/static/css/5.6a78eae4.chunk.css"
+  },
+  {
+    "revision": "1f520b3bdda66f147f96",
+    "url": "/BurgerBuilder/static/css/main.08cd1251.chunk.css"
+  },
+  {
+    "revision": "8942c86c9ddfcfb74ad1",
+    "url": "/BurgerBuilder/static/js/2.62ad9709.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BurgerBuilder/static/js/2.62ad9709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2363f3a3ab726c94c670",
+    "url": "/BurgerBuilder/static/js/3.3d1623c5.chunk.js"
+  },
+  {
+    "revision": "e7f0034e654e14cd74d0",
+    "url": "/BurgerBuilder/static/js/4.f7e9212e.chunk.js"
+  },
+  {
+    "revision": "9903bbdc8dbfb9af23f5",
+    "url": "/BurgerBuilder/static/js/5.4a86f591.chunk.js"
+  },
+  {
+    "revision": "1f520b3bdda66f147f96",
+    "url": "/BurgerBuilder/static/js/main.7cbad40f.chunk.js"
+  },
+  {
+    "revision": "afaa27eb83fbaba09c01",
+    "url": "/BurgerBuilder/static/js/runtime-main.382b5f4e.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerBuilder/static/media/original.b8503d26.png"
+  }
+]);
